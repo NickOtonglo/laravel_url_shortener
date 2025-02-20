c7157d61d66bed886dca7c036569f15b7bce3a6f
Nick Otonglo <nickotonglo@gmail.com>
2025-02-20 16:24:42 +0000

    added Github link to navbar Github icon

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <h2 class="app-title"><a href=" {{route('index')}} ">{{ config('app.name') }}</a></h2>
         <div class="icon">
-            <a href="/github.com"><i class="fa-brands fa-github"></i></a>
+            <a href="https://github.com/NickOtonglo/laravel_url_shortener"><i class="fa-brands fa-github"></i></a>
         </div>
     </div>
 </nav>
